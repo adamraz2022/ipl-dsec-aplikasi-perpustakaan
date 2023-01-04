a8ed1fbee10174dfa306c27b862784007ce16e59
ilhamfathurrohman20 <ilhamfathurrohman20@if.unjani.ac.id>
2023-01-04 08:35:20 +0700

    Add class Koneksi.java

---
+++ Interface/src/Koneksi.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ilham Fathurrohman
+ */
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ilham Fathurrohman
+ */
+public class Koneksi {
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.Statement;
+    import javax.swing.JOptionPane;
+ 
+public class koneksi {
+    Connection con;
+    Statement stm;
+    
+    public void config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/", "root", "");
+            stm = con.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "koneksi gagal "+e.getMessage());
+        }
+    }
+}
+}
